2eefc59db1f0c151a199ddcf66c16ec7fd594c92
전영빈 <kasdf12347@naver.com>
2023-10-23 10:21:36 +0900

    github에 프로젝트 올려놨습니다. fork해가세요

---
+++ src/test/java/com/playdata/gittest/GitTestMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
